cf785c02af0b0f8859664c87fea7ed7646e5304c
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2026-06-02 13:04:45 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+import pickle
+import streamlit as st
+import requests
+import pandas as pd
+
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()
+    data = response.json()
+    poster_path = data.get('poster_path')
+    if poster_path:
+        return "https://image.tmdb.org/t/p/w500/" + poster_path
+    return "https://via.placeholder.com/500x750?text=No+Poster"
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names, recommended_movie_posters
+
+
+st.header('Movie Recommender System')
+movies = pickle.load(open('movie_list.pkl','rb'))
+
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    
+    cols = st.columns(5)
+    
+    for i in range(5):
+        with cols[i]:
+            st.markdown(
+                f"""
+                <div style="text-align: center;">
+                    <img src="{recommended_movie_posters[i]}" style="width: 100%; border-radius: 8px;">
+                    <p style="font-weight: bold; margin-top: 5px; font-size: 16px;">{recommended_movie_names[i]}</p>
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
